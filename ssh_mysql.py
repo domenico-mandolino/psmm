b88238cf2afbc37e0b13b55de9efb2f962fc5bed
domenico-mandolino <146713339+domenico-mandolino@users.noreply.github.com>
2024-10-05 09:32:29 +0200

    Update ssh_mysql.py

--- ssh_mysql.py
+++ ssh_mysql.py
@@ -8,7 +8,7 @@ def connect_to_database():
             host='192.168.10.152', # Adresse de votre serveur MariaDB 
             database='plateflop', # Nom de votre base de données 
             user='dome', # Nom d'utilisateur 
-            password='x' # Remplacez par le mot de passe de l'utilisateur
+            password='' # Remplacez par le mot de passe de l'utilisateur
         ) 
         if connection.is_connected(): 
             print("Connexion réussie à la base de données")
